a659afb1d4770790adf1f43a18bf82f4c06ad331
2048io-png <justingabrielmreble@gmail.com>
2024-11-04 13:36:42 +0800

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -1,12 +1,12 @@
 addLayer("p", {
-    name: "fish", // This is optional, only used in a few places, If absent it just uses the layer id.
-    symbol: ".", // This appears on the layer's node. Default is the id with the first letter capitalized
+    name: "prestige", // This is optional, only used in a few places, If absent it just uses the layer id.
+    symbol: "p", // This appears on the layer's node. Default is the id with the first letter capitalized
     position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
     startData() { return {
         unlocked: true,
 		points: new Decimal(0),
     }},
-    color: "#223342",
+    color: "#00ffff",
     requires: new Decimal(10), // Can be a function that takes requirement increases into account
     resource: "prestige points", // Name of prestige currency
     baseResource: "points", // Name of resource prestige is based on
@@ -24,5 +24,24 @@ addLayer("p", {
     hotkeys: [
         {key: "p", description: "P: Reset for prestige points", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
     ],
+    upgrades: {
+		11: {
+			title: "p11",
+			description: "x2 point gain.",
+			cost: new Decimal(1)
+		},
+		12: {
+			title: "p12",
+			description: "Add +1 to the base point gain.",
+			cost: new Decimal(1),
+            unlocked() {return hasUpgrade('p',11)
+		},
+		13: {
+			title: "p13",
+			description: "x1.6 point gain.",
+			cost: new Decimal(3),
+            unlocked() {return hasUpgrade('p',12)
+		},
+	},
     layerShown(){return true}
 })
